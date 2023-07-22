73bf3bcdd2b18d88d4432e34c9c2641886387548
ChristopherSD <christopherDick00@gmail.com>
2023-07-22 18:37:06 +0200

    Add init script

---
+++ scripts/init.js
@@ -0,0 +1,11 @@
+import { SystemManager } from "./system-manager";
+import {MODULE, REQUIRED_CORE_MODULE_VERSION} from "./constants.js";
+
+Hooks.on('tokenActionHudCoreApiReady', async () => {
+    const module = game.modules.get(MODULE.ID);
+    module.api = {
+        requiredCoreModuleVersion: REQUIRED_CORE_MODULE_VERSION,
+        SystemManager
+    }
+    Hooks.call('requiredCoreModuleVersion', module);
+})
